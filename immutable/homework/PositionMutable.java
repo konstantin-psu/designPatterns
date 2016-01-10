578a0bdd1459ce60ac3264660b23f4d066803609
Konstantin Macarenco <konstan2@pdx.edu>
2016-01-10 15:15:03 -0800

    asdf

---
+++ immutable/homework/PositionMutable.java
@@ -0,0 +1,8 @@
+/**
+ * Created by konstantin on 08/01/16.
+ */
+public class PositionMutable extends Position {
+    public PositionMutable(int x, int y) {super(x,y);}
+    public void setX(int x) {this.x = x;}
+    public void setY(int x) {this.y = x;}
+}
