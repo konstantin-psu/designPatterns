7e3f64c88d0edf10f99d9002d3b81b78537e498f
Konstantin Macarenco <kmacarenco@tripwire.com>
2016-01-15 14:40:17 -0800

    Finished Proxy

---
+++ proxy/homework/Client.java
@@ -0,0 +1,18 @@
+/**
+ *  This class exercises a proxy of a (remote) calculator.
+ */
+class Client {
+
+    /**
+     *  Usual main, command line arguments are ignored.
+     *  Create remote calculator proxy,
+     *  request an addition, and print the result.
+     *  @param ignore Ditto.
+     */
+    public static void main (String [] ignore) {
+        System.setProperty("java.security.policy","file:///localhome/konstantin/githubRepos/designPatterns/proxy/homework/grant.policy");
+        Proxy proxy = new Proxy ();
+        float result = proxy.add (2.2f, 6.6f);
+        System.out.println ("Client: Calculator result: " + result);
+    }
+}
