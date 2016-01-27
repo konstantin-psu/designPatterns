41b434ebe16dcf635655708ab3d793f0ee3ff9bd
Konstantin Macarenco <konstan2@pdx.edu>
2016-01-26 17:38:30 -0800

    Added facade

--- iterator/homework/Client.java
+++ iterator/homework/Client.java
@@ -4,10 +4,10 @@
 public class Client {
     public static void main(String [] ignored) {
         int [] testdata = { 25, 40, 35, 30, 10, 20, 15, 50, 45, };
-        BinaryTree<Integer> tree = new Leaf<Integer>();
+        Tree<Integer> tree = new Tree<Integer>();
         for (int i: testdata) {
             System.out.println("inserting "+i);
-            tree = tree.insert(i);
+            tree.insert(i);
         }
 
         tree.print();
--- iterator/homework/Tree.java
+++ iterator/homework/Tree.java
@@ -1,5 +1,12 @@
 /**
  * Created by konstantin on 1/26/16.
  */
-public class Tree<E> {
+public class Tree<E extends Comparable<E>> {
+    private BinaryTree<E> treeRoot = new Leaf<E>();
+    public void insert(E x) {
+        treeRoot = treeRoot.insert(x);
+    }
+    public void print() {
+        treeRoot.print();
+    }
 }
